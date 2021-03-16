c448644351afbefb50b8906d49375b321c2e00e5
Becky Newton <iambexnewton@gmail.com>
2021-03-16 21:18:59 +0000

    updates

--- day-2/data-variables-expressions/exercises/variables.js
+++ day-2/data-variables-expressions/exercises/variables.js
@@ -15,3 +15,9 @@ console.log(email);
 
 let message = `Hello my name is ${firstName} ${lastName} and I'm ${age} years old.`;
 console.log(message);
+
+const fullName = `${firstName} ${lastName}`;
+
+const birthdayInfo = `My name is ${fullName} and I was born in $new Date().getFullYear() - age`;
+
+const petInfo = hasPets ? "I have pets" : "I don't have pets";
