68783bee90ef08483fde41c76567c47a54a01e9c
Becky Newton <iambexnewton@gmail.com>
2021-03-18 11:12:46 +0000

    day-4-objects-questions/2.js

--- day-4/objects/questions/2.js
+++ day-4/objects/questions/2.js
@@ -1,8 +1,8 @@
 const obj = {};
 
-obj.push('pat')
+obj.push("pat");
 
 console.log(obj);
 
-// What will happen when this file is executed with NodeJS ?
-// What is the problem with this code ?
+// What will happen when this file is executed with NodeJS ? type error
+// What is the problem with this code ? you can not push on an object its array method
