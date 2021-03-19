e3c839cf99e7f159d50fe5dcf549b10a21a2bc65
Becky Newton <iambexnewton@gmail.com>
2021-03-19 15:56:53 +0000

    day5export-examples

--- day-5/exports-and-require/index.js
+++ day-5/exports-and-require/index.js
@@ -5,4 +5,11 @@ const somethingFromFile4 = require("./examples/file-4"); //{add [function: add]}
 const somethingFromFile5 = require("./examples/file-5"); //{ printHello: [Function: printHello], add: [Function: add] }
 const somethingFromFile6 = require("./examples/file-6"); //{}
 
-console.log(somethingFromFile6);
+//1. Using only `somethingFromFile2` (_not_ file-1!), get `Vel` printed to the console
+console.log(somethingFromFile2.tutor);
+
+//2. Using only `somethingFromFile3`, get `Hello` printed to the console
+console.log(somethingFromFile3());
+
+//3. Now using only `somethingFromFile5` (not file-3!), get `Hello` printed to the console
+console.log(somethingFromFile5.printHello());
