bf2641d1d20e47addaad6b99a8f4f0535d0f9afd
Becky Newton <iambexnewton@gmail.com>
2021-03-19 15:43:32 +0000

    day-5-exports-index

--- day-5/exports-and-require/index.js
+++ day-5/exports-and-require/index.js
@@ -1,8 +1,8 @@
-const somethingFromFile1 = require('./examples/file-1');
-const somethingFromFile2 = require('./examples/file-2');
-const somethingFromFile3 = require('./examples/file-3');
-const somethingFromFile4 = require('./examples/file-4');
-const somethingFromFile5 = require('./examples/file-5');
-const somethingFromFile6 = require('./examples/file-6');
+const somethingFromFile1 = require("./examples/file-1"); //Vel
+const somethingFromFile2 = require("./examples/file-2"); //{tutor: Vel}
+const somethingFromFile3 = require("./examples/file-3"); //hello object
+const somethingFromFile4 = require("./examples/file-4"); //{add [function: add]}
+const somethingFromFile5 = require("./examples/file-5"); //{ printHello: [Function: printHello], add: [Function: add] }
+const somethingFromFile6 = require("./examples/file-6"); //{}
 
-//console.log(REPLACE_ME_WITH_A_VARIABLE_FROM_ABOVE);
+console.log(somethingFromFile6);
